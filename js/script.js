70e625b321659a948f22817e0b6edd65acfcf31b
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-02 15:34:03 +0100

    Stampo in console i numeri da 1 a 100

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// 1. stampo in pagina i numeri da 1 a 100
+for ( var i = 1; i <= 100; i++){
+  console.log(i);
+  // 2. controllo quali sono i numeri multipli di 3, 5 ed entrambi e cambio il loro risultato
+  
+}
