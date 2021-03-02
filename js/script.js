855de6b296a63903f5d3628fc78d58fdf1da26fb
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-02 16:30:21 +0100

    Modifica ciclo per visualizzare FizzBuzz per i numeri divisibili per 15

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 // 1. stampo in pagina i numeri da 1 a 100
-for ( var i = 1; i <= 100; i++){
-  // 2. controllo i numeri divisibili per 3 e sostituisco il loro risultato con la stringa Fizz
-  if (i % 3 == 0 ){
+for ( var i = 1; i < 101; i++){
+  // 2. controllo i numeri divisibili per 3 e 5 e sostituisco il loro risultato con la stringa FizzBuzz
+  if ((i % 3 == 0) && (i % 5 == 0)){
+    console.log("FizzBuzz");
+  }
+  // 3. controllo i numeri divisibili per 3 e sostituisco il loro risultato con la stringa Fizz
+  else if (i % 3 == 0 ) {
     console.log("Fizz");
   }
-  // 3. controllo i numeri divisibili per 5 e sostituisco il loro risultato con la stringa Buzz
+  // 4. controllo i numeri divisibili per 5 sostituisco il loro risultato con la stringa Buzz
   else if (i % 5 == 0){
     console.log("Buzz");
   }
-  // 4. controllo i numeri divisibili per 3 e 5 sostituisco il loro risultato con la stringa FizzBuzz
-   else if (i % 3 == 0 && i % 5 == 0){
-    console.log("FizzBuzz");
-  }
-   // 5. vengono stampati i numeri restanti
+   // 5. vengono stampati i restanti numeri
   else {
     console.log(i);
   }
